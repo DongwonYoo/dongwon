84c6c916659dc6582bcc529d7f07bbf9b71e7050
Yoo DongWon <d.yoo@ad.gxp.jp>
2023-04-17 19:13:05 +0900

    test

---
+++ practice/Review/conditional.js
@@ -0,0 +1,17 @@
+const age = parseInt( prompt("How old are you?"));
+
+//Int型
+//parseInt()
+
+
+//ArgumentがNaN(Not a Number)の場合trueをかえす
+//isNaN()
+console.log(isNaN(age));
+const chack = isNaN(age);
+if (age >= 20){
+    console.log("You can Dinkg!")
+}else if (chack == true ){
+    console.log("Please write a number!")
+}else {
+    console.log("Sorry.. You are too young")
+};  
