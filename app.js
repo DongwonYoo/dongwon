e8fbf27861f7f7257dd00f14c4f930cd2d0f801a
Yoo DongWon <d.yoo@ad.gxp.jp>
2023-04-18 16:50:36 +0900

    23/04/18_2

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input"); 
+const greeting = document.querySelector("#greeting");
+
+//stringを１回だけ書くため、
+
+//javascriptはstringを間違えても教えてくれないが、変数名が間違えたら教えてくれるだ
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+
+    localStorage.setItem("USERNAME_KEY",username)
+    // greeting.innerText = "Hello " + username ;
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+
+const savedUsername = localStorage.getItem("USERNAME_KEY");
+if (savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME); 
+    loginForm.addEventListener("submit", onLoginSubmit);
+
+
+}else{
+    paintGreetings(savedUsername);
+}
