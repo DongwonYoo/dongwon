313f34ca8e04e870cb4f4bcec7fea5390974ff81
Yoo DongWon <thd301302@gmail.com>
2023-04-19 17:20:46 +0900

    quotes/background/todo1

--- js/greetings.js
+++ js/greetings.js
@@ -1,5 +1,5 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input"); 
+const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 //stringを１回だけ書くため、
@@ -8,29 +8,25 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
 
-function onLoginSubmit(event){
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-
-    localStorage.setItem("USERNAME_KEY",username)
-    // greeting.innerText = "Hello " + username ;
-    paintGreetings(username);
+  localStorage.setItem("USERNAME_KEY", username);
+  // greeting.innerText = "Hello " + username ;
+  paintGreetings(username);
 }
 
-function paintGreetings(username){
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
 
 const savedUsername = localStorage.getItem("USERNAME_KEY");
-if (savedUsername === null){
-    loginForm.classList.remove(HIDDEN_CLASSNAME); 
-    loginForm.addEventListener("submit", onLoginSubmit);
-
-
-}else{
-    paintGreetings(savedUsername);
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
 }
