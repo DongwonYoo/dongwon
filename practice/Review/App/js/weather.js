4be58fb0db3729d37956d6eea7e4ba585079bfc1
Yoo DongWon <thd301302@gmail.com>
2023-04-21 20:53:37 +0900

    機能完成

--- practice/Review/App/js/weather.js
+++ practice/Review/App/js/weather.js
@@ -3,14 +3,26 @@ const API_KEY = "e70bc2431f2c4ffb506b4c9ee413ccf2";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
+
+  //API呼び出しURL
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  //API呼び出し
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
+      //Iconコード
+      const weatherIconCode = data.weather[0].icon;
+
+      //HTML Element
+      const city = document.querySelector("#weather span:first-child");
+      const weatherIcon = document.querySelector("#weather span:nth-child(2)");
+      const weather = document.querySelector("#weather span:last-child");
+
+      //HTML ElementへAPI
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}}`;
+      weatherIcon.innerHTML = `<img src="./img/icons/${weatherIconCode}.png">`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
     });
 }
 function onGeoError() {
--- practice/Review/function.js
+++ practice/Review/function.js
@@ -1,36 +1,30 @@
-
-
-function plus ( firstNumber, secondNumber){
-    console.log( "値は " + (firstNumber+secondNumber));
+function plus(firstNumber, secondNumber) {
+  console.log("値は " + (firstNumber + secondNumber));
 }
 //FunctionのArguments名はFunctionの中だけ
 
-function divide( firstNumber, secondNumber){
-    console.log( firstNumber / secondNumber);
+function divide(firstNumber, secondNumber) {
+  console.log(firstNumber / secondNumber);
 }
 plus(10, 2);
-plus(300, 10)
-divide(10,2);
+plus(300, 10);
+divide(10, 2);
 
-
-const player =  {
-    name: "Dongwon",
-    //objectの中でFunctionの宣言はちがう
-    sayHello: function(otherPersonName){
-        console.log("Hello! " + otherPersonName);
-    },
-
+const player = {
+  name: "Dongwon",
+  //objectの中でFunctionの宣言はちがう
+  sayHello: function (otherPersonName) {
+    console.log("Hello! " + otherPersonName);
+  },
 };
 console.log(player.name);
 player.sayHello("Mark");
 player.sayHello("Jin");
 
-
 const age = 29;
-function calculateKrAge(ageOfForeigner){
-    return ageOfForeigner +2 ;
+function calculateKrAge(ageOfForeigner) {
+  return ageOfForeigner + 2;
 }
 //returnしたらFunctionは終了
 const krAge = calculateKrAge(age);
 console.log(krAge);
-
