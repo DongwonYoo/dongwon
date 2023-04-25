37bd8bf12602a444beff1797232598c9d368d03c
Yoo DongWon <thd301302@gmail.com>
2023-04-25 19:54:46 +0900

    image_up

--- paint/app.js
+++ paint/app.js
@@ -1,54 +1,103 @@
+const fileInput = document.getElementById("file");
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+); //Array.fromでArrayに変換
+const color = document.querySelector("#color");
+const lineWidth = document.querySelector("#line-width");
 const canvas = document.querySelector("canvas");
-
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
 
-const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#c56cf0",
-  "#ff9f1a",
-  "#32ff7e",
-  "#7efff5",
-  "#18dcff",
-  "#7d5fff",
-];
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
-ctx.lineWidth = 2; //굵기
-ctx.moveTo(0, 0); //시작점
-function onclick(event) {
-  ctx.beginPath(); //초기화
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY); //마우스 위치
-  ctx.stroke();
+ctx.lineWidth = lineWidth.value;
+
+let isPainting = false;
+let isFilling = false;
+
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.beginPath();
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("mousemove", onclick);
+function startPainting() {
+  isPainting = true;
+}
 
-//家
-// ctx.fillRect(200, 200, 50, 200);
-// ctx.fillRect(400, 200, 50, 200);
-// ctx.lineWidth = 2;
-// ctx.fillRect(300, 300, 50, 100);
-// ctx.fillRect(200, 200, 200, 20);
-// ctx.moveTo(200, 200);
-// ctx.lineTo(325, 100);
-// ctx.lineTo(450, 200);
-// ctx.fill();
-//person
-// ctx.fillRect(210 - 50, 200 - 20, 15, 100);
-// ctx.fillRect(350 - 50, 200 - 20, 15, 100);
-// ctx.fillRect(260 - 50, 200 - 20, 60, 200);
+function cancelPainting() {
+  isPainting = false;
+}
 
-// ctx.arc(290 - 50, 150 - 20, 50, 0, 2 * Math.PI);
-// ctx.fill();
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
 
-// ctx.beginPath();
-// ctx.fillStyle = "white";
-// ctx.arc(270 - 50, 140 - 20, 8, Math.PI, 2 * Math.PI);
-// ctx.arc(305 - 50, 140 - 20, 8, Math.PI, 2 * Math.PI);
-// ctx.fill();
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = event.target.dataset.color;
+  ctx.fillStyle = event.target.dataset.color;
+  color.value = colorValue;
+}
+
+function onmModeClick(event) {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+function onDestroyClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+function onEraserClick(event) {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill";
+}
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  };
+}
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onmModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
